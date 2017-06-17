428e2e78fddfd675f8de1160059de35fab5cabc4
Elliott.zheng <elliott.zheng@foxmail.com>
2017-06-17 16:32:03 +0800

    before continus camera capture

--- Aruc/source.cpp
+++ Aruc/source.cpp
@@ -5,14 +5,23 @@
 using namespace std;
 using namespace cv;
 
-int main()
-{
-	Mat markerImage;
-	Ptr<cv::aruco::Dictionary> dictionary = cv::aruco::getPredefinedDictionary(cv::aruco::DICT_6X6_250);
-	for (int i = 0; i < 20; i++) {
-		aruco::drawMarker(dictionary, i, 1000, markerImage, 1);
-		imwrite(to_string(i)+".jpg", markerImage);
+int main(){
+	for (int i = 0; i < 5; i++){
+		string a = "a";
+		a = a + to_string(i);
+		cout << a +".jpg"<< endl;
+		Mat inputImage = imread(a+".jpg");
+		if (!inputImage.data){
+			cout << "can't find image" << endl;
+			return -1;
+		}
+		std::vector<int> markerIds;
+		std::vector<std::vector<Point2f>> markerCorners, rejectedCandidates;
+		Ptr<aruco::DetectorParameters> parameters = aruco::DetectorParameters::create();
+		Ptr<aruco::Dictionary> dictionary = aruco::getPredefinedDictionary(aruco::DICT_6X6_250);
+		aruco::detectMarkers(inputImage, dictionary, markerCorners, markerIds, parameters, rejectedCandidates);
+		aruco::drawDetectedMarkers(inputImage, markerCorners, markerIds);
+		imwrite(a+".png", inputImage);
 	}
-	
 	cvWaitKey(0);
 }
